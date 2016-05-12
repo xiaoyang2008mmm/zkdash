91fb84324ff5d669839031148cb0804e2be524d5
xiaoyang2008mmm <xiaoyang2008mmm@163.com>
2016-05-12 11:18:39 +0800

    修改js

--- static/js/zhangyue.js
+++ static/js/zhangyue.js
@@ -1,26 +1,43 @@
 var setting = {};
 $(document).ready(function() {
 
-    $.get("/get_base_node/",function(data,status){
-      $.fn.zTree.init($("#treeDemo"), setting, JSON.parse(data));
-     //alert("Data: " + typeof(JSON.parse(data)) +typeof(zNodes)+ "nStatus: " + status);
-     //alert("Data: " + typeof(JSON.parse(data)) +typeof(zNodes)+ "nStatus: " + status);
+    $.get("/get_base_node/",
+    function(data, status) {
+        $.fn.zTree.init($("#treeDemo"), setting, JSON.parse(data));
+        //alert("Data: " + typeof(JSON.parse(data)) +typeof(zNodes)+ "nStatus: " + status);
     });
 
-////////////////////////
-$("#node_search").click(function() {
-    if ($("#node_path").val() == "") {
-        alert("输入首节点路劲");
-    } else {
-        $.post("/node_path/", {
-            node_path: $("#node_path").val(),
-        },
-        function(data) {
-            $.fn.zTree.init($("#treeDemo"), setting, JSON.parse(data));
+    ////////////////////////
+    $("#node_search").click(function() {
+        if ($("#node_path").val() == "") {
+            alert("输入首节点路劲");
+        } else {
+            $.post("/node_path/", {
+                node_path: $("#node_path").val(),
+            },
+            function(data) {
+                $.fn.zTree.init($("#treeDemo"), setting, JSON.parse(data));
 
-        });
-    };
-});
-////////////////////////
+            });
+        };
+    });
+    ////////////////////////
+    //
+    $("#delete_btn").click(function() {
+        alert("wdqwdq");
+
+        var treeObj = $.fn.zTree.getZTreeObj("treeDemo");
+        var nodes = treeObj.getSelectedNodes();
+
+        treeNode = nodes[0];
+        if (nodes.length == 0) {
+            alert("请先选择一个节点");
+            return;
+        }
+        for (var i = 0,
+        l = nodes.length; i < l; i++) {
+            treeObj.removeNode(nodes[i]);
+        }
+    });
 
 });
