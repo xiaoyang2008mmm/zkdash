642efc7a567a4fefa94138fd491eaa33828ba5df
xiaoyang2008mmm <xiaoyang2008mmm@163.com>
2016-05-16 21:05:57 +0800

    修改modles

--- modle/syncdb.py
+++ modle/syncdb.py
@@ -16,11 +16,11 @@ class BaseModel(Model):
         database = database
 
 class ZdSnapshot(BaseModel):
-    #id = IntegerField(primary_key=True, constraints=[SQL("AUTO_INCREMENT")])
+    id = IntegerField(primary_key=True, constraints=[SQL("AUTO_INCREMENT")])
     cluster_name = CharField(max_length=64, null=True)
     path = CharField(max_length=512, null=True)
     data = CharField(null=True)
-    #create_time = DateTimeField(null=True)
+    create_time = DateTimeField(null=True)
 
     class Meta(object):
 
@@ -28,11 +28,27 @@ class ZdSnapshot(BaseModel):
         """
         db_table = "zd_snapshot"
 
+class ZdZookeeper(BaseModel):
+
+
+    id = IntegerField(primary_key=True, constraints=[SQL("AUTO_INCREMENT")])
+    cluster_name = CharField(max_length=128,null=True)
+    hosts = CharField(max_length=128,null=True)
+    business = CharField(max_length=255,null=True)
+
+    class Meta(object):
+
+        db_table = "zd_zookeeper"
+
+
 
 
 if __name__ == "__main__":
     try:
         ZdSnapshot.create_table()    
-    except peewee.OperationalError:
+    except OperationalError:
         print "ZdSnapshot table already exists!"
-
+    try:
+        ZdZookeeper.create_table()    
+    except OperationalError:
+        print "ZdZookeeper table already exists!"
