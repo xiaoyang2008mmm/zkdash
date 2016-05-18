908790edf7f63a039087dd40019d1fdb5a41bbb1
root <root@localhost.(none)>
2016-05-18 19:52:45 -0400

    代码优化

--- lib/parserconf.py
+++ lib/parserconf.py
@@ -1,12 +1,5 @@
 import ConfigParser
 
-def get_zk_server(file="./conf/zookeeper.conf"):
-
-    cf = ConfigParser.ConfigParser()    
-    cf.read(file)   
-  
-    zk_server = cf.get("zk", "server")    
-    return zk_server
 
 
 
